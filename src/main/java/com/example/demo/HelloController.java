7f2a6f34fb2b5566059c12eff8bd3e21bd74c6b1
FarhadPathan <farhad.nbrict@gmail.com>
2024-11-07 14:49:11 +0600

    initial commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -19,4 +14,10 @@ class HelloController {
     public String greetWithPost(@RequestBody NameRequest nameRequest) {
         return "Hello, " + nameRequest.getName() + "!";
     }
+
+    @PutMapping
+    public String greetWithPut(@RequestBody NameRequest nameRequest) {
+        return String.format("Hello %s %s %s", nameRequest.getName() , nameRequest.getLastName() , nameRequest.getAge());
+    }
+
 }
--- src/main/java/com/example/demo/NameRequest.java
+++ src/main/java/com/example/demo/NameRequest.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
 class NameRequest {
-    private String name;
+    private String name, lastName, age, email;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) { this.age = age; }
+
+
+
+
 }
